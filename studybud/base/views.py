ed9436e450bd41a8b36312bc309b3f07e08b7823
tticjswo <tticjswo@naver.com>
2022-04-01 19:23:58 +0900

    [update] 댓글 기능 / Activity_feed / User_Profile / Static file 완료 -- Theme installation 진행중

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -3,13 +3,13 @@ from django.shortcuts import render, redirect
 from django.db.models import Q
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-#from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login , logout
 from django.contrib.auth.forms import UserCreationForm
 
 
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 # rooms =[
@@ -73,16 +73,50 @@ def home(request) :
         Q(name__icontains= q) |
         Q(description__icontains= q)
     ) #
+
     topics = Topic.objects.all()
     room_count = rooms.count()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
-    context = {'rooms':rooms, 'topics':topics, 'room_count' : room_count}
+    context = {'rooms':rooms, 'topics':topics, 'room_count' : room_count, 'room_messages' : room_messages}
     return render(request,'base/home.html',context)
+
+def userProfile(request,pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user':user, 'rooms':rooms,'room_messages':room_messages,'topics':topics}
+    return render(request,'base/profile.html',context)
 
 def room(request, pk) :
     room = Room.objects.get(id=pk)
-    context = {'room':room}
+    room_messages = room.message_set.all()
+    participants = room.participants.all()
+
+    if request.method =='POST' :
+        message = Message.objects.create(
+            user = request.user,
+            room = room, 
+            body = request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room',pk = room.id)
+
+    context = {'room':room, 'room_messages' : room_messages, 'participants' :participants}
     return render(request,'base/room.html',context)
+
+@login_required(login_url = '/login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user :
+        return HttpResponse('You are not allowed here!!')
+
+    if request.method == 'POST' :  
+        message.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html',{'obj' : message})
 
 @login_required(login_url = '/login')
 def createRoom(request) :
@@ -90,7 +124,9 @@ def createRoom(request) :
     if request.method == 'POST' :
         form = RoomForm(request.POST)
         if form.is_valid():
-            form.save()
+            room = form.save(commit = False)
+            room.host = request.user
+            room.save()
             return redirect('home')
 
     context= { 'form' : form }
